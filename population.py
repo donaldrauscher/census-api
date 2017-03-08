0d13ae2e368426451272015b4824ff96c85f2aed
Donald Rauscher <donald.rauscher@gmail.com>
2017-03-08 16:37:59 -0600

    General code clean-up

--- population.py
+++ population.py
@@ -10,6 +10,9 @@ with open('key.yaml', 'r') as f:
 
 key = key['key']
 
+# variables to pull
+variables = {'P0010001':'population'}
+
 # convert json results into pandas df
 def to_pd(j):
 	obj = json.loads(j)
@@ -19,23 +22,24 @@ def to_pd(j):
 	return df
 
 # download states
-states = requests.get(url, params={'get':'P0010001,NAME', 'for':'state:*', 'key':key})
+states = requests.get(url, params={'get':','.join([k for k,v in variables.items()]), 'for':'state:*', 'key':key})
 states = to_pd(states.text)
 
 # download zips for each state
 state_codes = states.state.tolist()
-zip_pop = list()
+zip_data = list()
 
 for sc in state_codes:
-	temp = requests.get(url, params={'get':'P0010001', 'for':'zip code tabulation area:*', 'in': 'state:%s' % str(sc).zfill(2), 'key':key})
+	temp = requests.get(url, params={'get':','.join([k for k,v in variables.items()]), 'for':'zip code tabulation area:*', 'in': 'state:%s' % str(sc).zfill(2), 'key':key})
 	temp_df = to_pd(temp.text)
-	zip_pop.append(temp_df)
+	zip_data.append(temp_df)
 
-zip_pop = pd.concat(zip_pop)
+zip_data = pd.concat(zip_data)
 
 # do some formatting
-zip_pop = zip_pop.rename(columns={'P0010001':'population', 'zip code tabulation area':'zip'})[['zip', 'population']]
-zip_pop['zip'] = zip_pop.zip.apply(lambda x: str(x).zfill(5))
+zip_data.rename(columns={**variables,'zip code tabulation area':'zip'}, inplace=True)
+zip_data = zip_data[['zip', 'population']]
+zip_data['zip'] = zip_data.zip.apply(lambda x: str(x).zfill(5))
 
 # export
-zip_pop.to_csv('./zip_pop.csv', index=False, quoting=csv.QUOTE_ALL)
+zip_data.to_csv('./outputs/population.csv', index=False, quoting=csv.QUOTE_ALL)
