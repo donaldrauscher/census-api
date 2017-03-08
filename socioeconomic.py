91d6334801c9f40876180c1b30e5fc3d19a36242
Donald Rauscher <donald.rauscher@gmail.com>
2017-03-08 17:23:23 -0600

    Remove mortgage; change median income to median household income

--- socioeconomic.py
+++ socioeconomic.py
@@ -15,13 +15,10 @@ variables = {
 	'NAME':'name',
 	'B07013_003E':'renter_numerator',
 	'B07013_001E':'renter_denominator',
-	'B25081_002E':'mortgage_numerator',
-	'B25081_001E':'mortgage_denominator',
-	'B07011_001E':'median_income',
+	'B19013_001E':'median_household_income',
 	'B07009_003E':'hs_graduate_numerator',
 	'B07009_005E':'college_graduate_numerator',
-	'B07009_001E':'education_denominator',
-	'B25010_001E':'household_size'
+	'B07009_001E':'education_denominator'
 }
 
 # convert json results into pandas df
@@ -40,17 +37,15 @@ zip_data.rename(columns={**variables,'zip code tabulation area':'zip'}, inplace=
 
 # calculate variables
 zip_data['renter'] = zip_data.renter_numerator/zip_data.renter_denominator
-zip_data['mortgage'] = zip_data.mortgage_numerator/zip_data.mortgage_denominator
 zip_data['hs_graduate'] = zip_data.hs_graduate_numerator/zip_data.education_denominator
 zip_data['college_graduate'] = zip_data.college_graduate_numerator/zip_data.education_denominator
 
 # null out variables without enough sample
 min_sample = 1000
 zip_data.loc[zip_data.renter_denominator < min_sample, 'renter'] = None
-zip_data.loc[zip_data.mortgage_denominator < min_sample, 'mortgage'] = None
 zip_data.loc[zip_data.education_denominator < min_sample, ['hs_graduate','college_graduate']] = None
 
 # export
 zip_data['zip'] = zip_data.zip.apply(lambda x: str(x).zfill(5))
-zip_data = zip_data[['zip','renter','mortgage','hs_graduate','college_graduate','median_income','household_size']]
+zip_data = zip_data[['zip','renter','hs_graduate','college_graduate','median_household_income']]
 zip_data.to_csv('./outputs/socioeconomic.csv', index=False, quoting=csv.QUOTE_ALL)
